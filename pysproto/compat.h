b9ac376559410a78498eea09c9a0f5811aba76cf
tangzhaoning <yuxiaichou@gmail.com>
2019-11-19 14:34:05 +0800

    只支持python3

--- pysproto/compat.h
+++ pysproto/compat.h
@@ -3,17 +3,12 @@
 typedef void (*capsule_dest)(PyObject *);
 typedef void (*cobj_dest)(void *);
 
-#if PY_MAJOR_VERSION != 2
-    #error "need python 2.*"
-#elif PY_MINOR_VERSION > 6
-    #define CAP_NEW PyCapsule_New
-    #define DEST_FUNC_TYPE capsule_dest
-    #define CAP_GET_POINTER PyCapsule_GetPointer
-#else
-    #define CAP_NEW(a,b,c) PyCObject_FromVoidPtr(a,c)
-    #define DEST_FUNC_TYPE cobj_dest
-    #define CAP_GET_POINTER(a,b) PyCObject_AsVoidPtr(a)
-#endif
+#if PY_MAJOR_VERSION != 3
+    #error "need python 3.*"
+
+#define CAP_NEW PyCapsule_New
+#define DEST_FUNC_TYPE capsule_dest
+#define CAP_GET_POINTER PyCapsule_GetPointer
 
 PyObject* make_capsule(void *p, const char *name, capsule_dest dest) {
     return CAP_NEW(p, name, (DEST_FUNC_TYPE)dest);
